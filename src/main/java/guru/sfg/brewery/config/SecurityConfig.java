3c44aca44974cfc24356639b6df6b023e2b02169
Michael <mfile849@gmail.com>
2022-09-21 13:34:59 -0500

    Section 6 Completion
    Password Security

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package guru.sfg.brewery.config;
 
+import guru.sfg.brewery.security.SfgPasswordEncoderFactories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,11 +11,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return SfgPasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -37,15 +49,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("spring")
-                .password("{noop}mfile")
+                .password("{bcrypt}$2a$10$nhzYthyfTGokxHD2MIvLDuXmvZE0ZoDVU38B8cmFKoVh3oi9JgmPW")
                 .roles("ADMIN")
                 .and()
                 .withUser("user")
-                .password("{noop}password")
+                .password("{sha256}12a27a9475d6812ab323999c2fcf0cadef0f68055e15618e3cbc5aa8ad060af3c13dd6faa356854e")
                 .roles("USER")
                 .and()
                 .withUser("scott")
-                .password("{noop}tiger")
+                .password("{bcrypt15}$2a$15$1fEf.4pp1LgMWl1scD9WmO3vChmdChYCKMb3kms6078SGOij7q7.e")
                 .roles("CUSTOMER");
     }
 
--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -22,6 +22,15 @@ public class BeerControllerIT extends BaseIT{
     }
 
     @Test
+    void initCreationFormWithSpring() throws Exception{
+        mockMvc.perform(get("/beers/new")
+                        .with(httpBasic("spring", "mfile")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/createBeer"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
+    @Test
     void initCreationFormWithScott() throws Exception{
         mockMvc.perform(get("/beers/new")
                         .with(httpBasic("scott", "tiger")))
